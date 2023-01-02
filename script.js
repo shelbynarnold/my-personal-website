cf5869df4b073c8093f938f8d3d82ff1436ca2bd
shelby <shelbynarnold@yahoo.com>
2023-01-01 20:35:49 -0700

    Added bio/about me

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Welcome to my personal website! My name is Shelby.";
 document.querySelector("body").appendChild(h2);
+addEventListener('mouseover')
