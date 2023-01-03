d1a8c5ec0468b60ae35acfb01f813de1ef8791d4
shelby <shelbynarnold@yahoo.com>
2023-01-03 12:33:33 -0700

    added borders

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const para = document.createElement("div");
-para.innerHTML = "Whatever man"
-document.body.appendChild(para);
-
 const elem = document.getElementById("fenris")
-console.log(elem)
  elem.addEventListener('click', (event) => {
-    elem.style ="color:red"
+    if (elem.style.color === "red") {
+      elem.style ="color:black"
+       }
+       else {
+         elem.style = "color:red"
+       }
  })
