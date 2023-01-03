92d680e0c1bc795de7db2533c698882606ad9c83
shelby <shelbynarnold@yahoo.com>
2023-01-03 11:45:08 -0700

    added event listener

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Welcome to my personal website! My name is Shelby.";
-document.querySelector("body").appendChild(h2);
-addEventListener('mouseover')
+const para = document.createElement("div");
+para.innerHTML = "Whatever man"
+document.body.appendChild(para);
+
+const elem = document.getElementById("fenris")
+console.log(elem)
+ elem.addEventListener('click', (event) => {
+    elem.style ="color:red"
+ })
